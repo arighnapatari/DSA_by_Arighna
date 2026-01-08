c077f0e2b120fc220dd2e92475bf5eb4f4fbe751
arighnapatari <arighnapatari@gmail.com>
2026-01-08 14:56:11 +0530

    Create 06_Single_Number.cpp

---
+++ Arrays/06_Single_Number.cpp
@@ -0,0 +1,14 @@
+//Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+//Apprpoach --> XOR
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int XOR = 0;
+        for(int i = 0; i< nums.size(); i++){
+            XOR = XOR ^ nums[i];
+
+        }
+        return XOR;
+        
+    }
+};
