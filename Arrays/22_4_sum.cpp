74a0c93038893c54e125c056446bb2a3848b6416
arighnapatari <arighnapatari@gmail.com>
2026-01-17 22:55:24 +0530

    Create 22_4_sum.cpp

---
+++ Arrays/22_4_sum.cpp
@@ -0,0 +1,45 @@
+//Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+
+//0 <= a, b, c, d < n
+//a, b, c, and d are distinct.
+//nums[a] + nums[b] + nums[c] + nums[d] == target
+//You may return the answer in any order.
+// Approach--> Two Pointers
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+
+        for(int i=0; i<n; i++)
+        {
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            for(int j=i+1; j<n; j++)
+            {
+                if(j != i+1 && nums[j] == nums[j-1]) continue;
+                int k = j+1;
+                int l = n-1;
+                while(k<l)
+                {
+                    long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
+
+                    if(sum == target)
+                    {
+                        ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+                        k++;
+                        l--;
+                        while(k<l && nums[k] == nums[k-1]) k++;
+                        while(k<l && nums[l] == nums[l+1]) l--;
+                    }
+                    else if(sum < target) k++;
+                    else l--;
+
+                }
+            }
+        }
+        return ans;
+        
+    }
+};
