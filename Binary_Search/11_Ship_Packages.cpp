d0038d0edd8d10bc2f8d15f8bd9b52bc2bacba03
arighnapatari <arighnapatari@gmail.com>
2026-01-25 18:44:30 +0530

    Create 11_Ship_Packages.cpp

---
+++ Binary_Search/11_Ship_Packages.cpp
@@ -0,0 +1,43 @@
+/*A conveyor belt has packages that must be shipped from one port to another within days days.
+
+The ith package on the conveyor belt has a weight of weights[i]. Each day, we load the ship with packages on the conveyor belt (in the order given by weights). We may not load more weight than the maximum weight capacity of the ship.
+
+Return the least weight capacity of the ship that will result in all the packages on the conveyor belt being shipped within days days.*/
+
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int low = *max_element(weights.begin(), weights.end());
+        int high = accumulate(weights.begin(), weights.end(), 0);
+
+        while(low<=high)
+        {
+            int mid = low + (high-low) / 2;
+
+            int noofdays = findDays(weights, mid);
+            if(noofdays <= days)
+            {
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
+        return low;
+        
+    }
+private:
+    int findDays(vector<int>& weights,  int cap)
+    {
+        int days = 1, load = 0;
+
+        for(int i = 0; i<weights.size(); i++)
+        {
+            if(weights[i] + load > cap)
+            {
+                days++;
+                load = weights[i];
+            }
+            else load += weights[i];
+        }
+        return days;
+    }
+};
