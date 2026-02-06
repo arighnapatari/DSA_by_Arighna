e3d31b6106c6f7e70065ce26b88af21f0d9b35b9
arighnapatari <arighnapatari@gmail.com>
2026-02-06 21:53:05 +0530

    Create 12_Add_two_Numbers.cpp

---
+++ Linked_Lists/12_Add_two_Numbers.cpp
@@ -0,0 +1,38 @@
+/*You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.*/
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(-1);
+        ListNode* curr = dummy;
+        ListNode* t1 = l1;
+        ListNode* t2 = l2;
+        int carry = 0;
+        while(t1 != NULL || t2 != NULL)
+        {
+            int sum = carry;
+            if(t1) sum += t1->val;
+            if(t2) sum += t2->val;
+            ListNode* newNode = new ListNode(sum % 10);
+            carry = sum / 10;
+            curr -> next = newNode;
+            curr = curr->next;
+
+            if(t1) t1 = t1->next;
+            if(t2) t2 = t2->next;
+
+        }
+        if(carry) 
+        {
+            ListNode* newNode = new ListNode(carry);
+            curr->next = newNode;
+
+        }
+        return dummy->next;
+    }
+};
+
+ 
+
