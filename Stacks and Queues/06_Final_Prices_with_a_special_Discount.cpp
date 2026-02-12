fa9053c216788e5a4c789f85362b63d3ebc43c5a
Arighna Patari <arighnapatari@gmail.com>
2026-02-12 20:40:22 +0530

    Create 06_Final_Prices_with_a_special_Discount.cpp

---
+++ Stacks and Queues/06_Final_Prices_with_a_special_Discount.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        int n = prices.size();
+        vector<int> ans = prices;
+        stack<int> st;
+
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && prices[i] <= prices[st.top()]) {
+                int idx = st.top();
+                st.pop();
+                ans[idx] = prices[idx] - prices[i];
+            }
+            st.push(i);
+        }
+
+        return ans;
+    }
+};
