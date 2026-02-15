18781a02c424f7ab3916caade06d7ec0b655b1fd
Arighna Patari <arighnapatari@gmail.com>
2026-02-15 20:01:01 +0530

    Create 12_Letter_Combinations_of_a_Phone_number.cpp

---
+++ Recursion/12_Letter_Combinations_of_a_Phone_number.cpp
@@ -0,0 +1,42 @@
+//Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+//A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+/*Example 1:
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+Example 2:
+Input: digits = "2"
+Output: ["a","b","c"]*/
+
+class Solution {
+public:
+    vector<string> result;
+    void solve(int index, string digits, string &curr, vector<string> &mapping)
+    {   
+        if(index == digits.size())
+        {
+            result.push_back(curr);
+            return;
+
+        }
+        string letters = mapping[digits[index] - '0'];
+
+        for(char ch : letters)
+        {
+            curr.push_back(ch);
+            solve(index + 1, digits, curr, mapping);
+            curr.pop_back();
+
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        if(digits.empty()) return{};
+
+        vector<string> mapping = {
+            "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+        };
+        string curr = "";
+        solve(0, digits, curr, mapping);
+        return result;
+
+    }
+};
