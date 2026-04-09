98cf702c46d689a55f963a62739315291a11f4f5
Arighna Patari <arighnapatari@gmail.com>
2026-04-09 21:54:09 +0530

    Create 10_Sum_of_Subarray_Ranges.cpp

---
+++ Stacks and Queues/10_Sum_of_Subarray_Ranges.cpp
@@ -0,0 +1,57 @@
+/*You are given an integer array nums. The range of a subarray of nums is the difference between the largest and smallest element in the subarray.
+
+Return the sum of all subarray ranges of nums.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: 4
+Explanation: The 6 subarrays of nums are the following:
+[1], range = largest - smallest = 1 - 1 = 0 
+[2], range = 2 - 2 = 0
+[3], range = 3 - 3 = 0
+[1,2], range = 2 - 1 = 1
+[2,3], range = 3 - 2 = 1
+[1,2,3], range = 3 - 1 = 2
+So the sum of all ranges is 0 + 0 + 0 + 1 + 1 + 2 = 4.*/
+
+class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        int n = nums.size();
+        long long summin = 0, summax = 0;
+
+        stack<int> st;
+
+        for(int i = 0; i <= n; i++){
+            while(!st.empty() && (i == n || nums[st.top()] > nums[i])){
+                int mid = st.top(); st.pop();
+                int left = st.empty() ? -1 : st.top();
+                int right = i;
+
+                long long count = (mid - left) * (right - mid);
+                summin += count * nums[mid];
+
+            }
+            st.push(i);
+
+        }
+        while(!st.empty()) st.pop();
+
+        for(int i = 0; i <= n; i++){
+            while(!st.empty() && (i == n || nums[st.top()] < nums[i])){
+                int mid = st.top(); st.pop();
+                int left = st.empty() ? -1 : st.top();
+                int right = i;
+
+                long long count = (mid - left) * (right - mid);
+                summax += count * nums[mid];
+            }
+            st.push(i);
+        }
+        return summax - summin;
+
+    }
+};
